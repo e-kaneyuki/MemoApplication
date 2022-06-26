db495dba59dc26fb4361471dbab1d48a4a875404
e-kaneyuki <occultchikin@gmail.com>
2022-06-26 20:03:42 +0900

    TaskController のIndexメソッドでTask一覧が表示できるようにした

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function index() {
+        $task = Task::all();
+        return view('auth.task', compact('task'));
+    }
 }
---
+++ resources/views/auth/task.blade.php
@@ -0,0 +1,7 @@
+@section('title', 'Task管理ツール')
+
+@extends('layouts.application')
+
+@section('content')
+    <P>送られてきた変数は<br>{{ $task }}<br>です</P>
+@endsection
