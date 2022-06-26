d9050497f7db5c7776bbbc4b6f0cb59fd14ceda1
e-kaneyuki <occultchikin@gmail.com>
2022-06-26 20:04:33 +0900

    taskテーブルにデータ追加できるようにした

--- app/Models/task.php
+++ app/Models/task.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class task extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'task',
+        'memo',
+        'user_id',
+    ];
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }
