7fc854b616e6e386bf859e2219aff4450336d404
e-kaneyuki <occultchikin@gmail.com>
2022-06-28 13:47:39 +0900

    index_task.blade.phpの不要なコードと行間を削除

--- resources/views/auth/index_task.blade.php
+++ resources/views/auth/index_task.blade.php
@@ -3,12 +3,6 @@
 @extends('layouts.application')
 
 @section('content')
-{{--
-<h3>送られてきた変数は</h3>
-<h4 class="text-danger">{{ $task }}</h4>
-<h4 class="text-danger">{{ $task_memo }}</h4>
-<h3>です</h3>
---}}
 
 <h3>私のタスクは</h3>
 <table border="2">
@@ -28,9 +22,7 @@
         <td>{{$onetask['updated_at']}}</td>
     </tr>
     @endforeach
-
 </table>
-
 <h3>です</h3>
 
 @endsection
