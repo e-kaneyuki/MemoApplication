47968d06155ee998068bd3f726e54a9647af6f78
e-kaneyuki <occultchikin@gmail.com>
2022-07-06 01:22:23 +0900

    実施状況の条件分岐ができるようControllerでパラメータを作成、各メソッドのphpdocを作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,33 +9,61 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
+    /**
+     * Task一覧表示
+     * @param Illuminate\Database\Eloquent\Collection $collection
+     * @param Illuminate\Support\Collection $task_flg_base
+     * @param array boolean[] $array_task_flg trueは非実施 falseは実施済み
+     */
     public function index() {
         $user = Auth::user();
         $collection = $user->task()->get();
+        $task_flg_base = $collection->pluck('task_flg');
+        $array_task_flg = $task_flg_base->toArray();
+        $task_flg_zero = in_array(false,$array_task_flg);
+        $task_flg_one = in_array(true,$array_task_flg);
 
-        return view('auth.index_task', compact('collection'));
+        return view('auth.index_task', compact('collection','task_flg_zero','task_flg_one'));
     }
+    /**
+     * Task詳細表示
+     * @param App\Models\Task $id
+     * @var array boolean[] $array_task_flg
+     */
     public function show($id) {
         $pick_task = Task::find($id);
 
         return view('auth.show_task', compact('pick_task'));
     }
+    //Task新規作成画面への遷移
     public function create() {
         return view('auth.create_task');
     }
+    /**
+     * Task新規登録
+     * @param Illuminate\Http\Request  $request
+     * @param App\Models\Task $task
+     *
+     */
     public function store(Request $request) {
         $task = new Task;
         $task->task = $request->task;
         $task->memo = $request->memo;
         $task->user_id = Auth::id();
         $task->save();
-        return redirect('/index');
+        return redirect('/index')->with('message', 'タスクを作成しました');
     }
     public function edit($id) {
         $edit_task = Task::find($id);
         return view('auth.edit_task', compact('edit_task'));
     }
-
+    /**
+     * Task情報更新
+     *
+     * @param Illuminate\Http\Request
+     * @param int $id
+     *
+     */
     public function update(Request $request, $id) {
         $task = Task::find($id);
         $task->task = $request->task;
@@ -43,11 +71,29 @@ class TaskController extends Controller
         $task->task_flg = $request->task_flg;
         $task->user_id = Auth::id();
         $task->save();
-        return redirect()->route('show', ['id' => $id]);
+        return redirect()->route('show', ['id' => $id])->with('message', 'タスクを編集しました');
     }
     public function delete($id) {
         $task = Task::find($id);
         $task->delete();
-        return redirect('/index');
+        return redirect('/index')->with('delete_message', 'タスクを削除しました');
     }
+    /**
+     * Task実施状況でのSort
+     * @param Illuminate\Database\Eloquent\Collection $collection
+     * @param Illuminate\Support\Collection $task_flg_base
+     * @param array boolean[] $array_task_flg trueは非実施 falseは実施済み
+     *
+     */
+    public function search($task_fig) {
+        $user = Auth::user();
+        $collection = $user->task()->where('task_flg',$task_fig)->get();
+        $task_flg_base = $collection->pluck('task_flg');
+        $array_task_flg = $task_flg_base->toArray();
+        $task_flg_zero = in_array(false,$array_task_flg);
+        $task_flg_one = in_array(true,$array_task_flg);
+
+        return view('auth.index_task', compact('collection','task_flg_zero','task_flg_one'));
+    }
+
 }
