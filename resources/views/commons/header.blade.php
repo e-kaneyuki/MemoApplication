b6a875688055d0155169f6583b20fdf6a8073cce
e-kaneyuki <occultchikin@gmail.com>
2022-07-17 16:24:35 +0900

    アプリ起動時のHome画面を編集

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand text-dark" style="font-size:x-large;" href="/">
         セルフ管理ツール
     </a>
-
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
     @if (Auth::check())
         <div class="mt-3 space-y-1">
             <!-- Authentication -->
@@ -16,7 +16,11 @@
                 </x-responsive-nav-link>
             </form>
         </div>
-
+    @else
+    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">ログイン</a>
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">新規登録</a>
+        @endif
     @endif
-
+    </div>
 </header>
