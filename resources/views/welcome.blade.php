4ad4fd63736e09dcbaaf23528ec63d273814e39c
e-kaneyuki <occultchikin@gmail.com>
2022-07-18 18:33:01 +0900

    テキストを中央寄せ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,15 @@
 @extends('layouts.application')
 @section('content')
 <div>
-    <h2>これは、普段スケジュール管理が下手くそな私のために作ったアプリです！</h2>
+    <h2 style="text-align:center">これは、普段スケジュール管理が下手くそな私のために作ったアプリです！</h2>
     <br>
 </div>
 <div>
-    <h3>スケジュールは金行の<img src="../images/icons8-google-calendar-48.png" alt="サンプル画像">Googleカレンダーと連携させており共有が可能です</h3>
+    <h3 style="text-align:center">スケジュールは金行の<img src="../images/icons8-google-calendar-48.png" alt="サンプル画像">Googleカレンダーと連携させており共有が可能です</h3>
     <br>
 </div>
 <div>
-    <h4>★★ご要望があれば別の個人用のアプリも作成いたします★★</h4>
+    <h4 style="text-align:center">★★ご要望があれば別の個人用のアプリも作成いたします★★</h4>
     <br>
 </div>
 <br>
@@ -19,15 +19,17 @@
 
 <div>
 @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div  style="text-align: center" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <h4>ログイン済みです<br>
                         以下の一覧へお進みください</h4>
+                        <br>
                         <button type="button" class="btn btn-success btn-lg" style='margin-bottom: 10px' onclick="location.href='{{ route('task.index')}}'">タスク一覧へ</button>
                         <button type="button" class="btn btn-success btn-lg" style='margin-bottom: 10px' onclick="location.href='{{ route('place.index')}}'">観光リストへ</button>
                         <button type="button" class="btn btn-success btn-lg" style='margin-bottom: 10px' onclick="location.href='{{ route('index_task_google_calendar')}}'"><img src="../images/icons8-google-calendar-48.png" alt="サンプル画像"width="24" height="24">カレンダーリストへ</button>
                     @else
                         <h4>ログイン後はタスク一覧へ移ります</h4>
+                        <br>
                         <button type="button" class="btn btn-success btn-lg" onclick="location.href='{{ route('login')}}'">ログイン</button>
 
                         @if (Route::has('register'))
