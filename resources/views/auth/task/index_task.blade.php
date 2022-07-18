4dc51bdb64f11f6809beb65a5b31914331d05b80
e-kaneyuki <occultchikin@gmail.com>
2022-07-18 15:00:23 +0900

    タスク一覧のボタン修正とGoogleアイコンを導入

--- resources/views/auth/task/index_task.blade.php
+++ resources/views/auth/task/index_task.blade.php
@@ -7,11 +7,12 @@
 @elseif(session('delete_message'))
 <div class="alert alert-danger">{{session('delete_message')}}</div>
 @endif
+
 <h3>タスク一覧</h3>
 <br>
 <div>
-    <button type="button" style='margin-bottom: 10px' onclick="location.href='{{ route('place.index')}}'">観光リストへ</button>
-    <button type="button" style='margin-bottom: 10px' onclick="location.href='{{ route('index_task_google_calendar')}}'">カレンダーリストへ</button>
+    <button type="button" class="btn btn-success btn-lg" style='margin-bottom: 10px' onclick="location.href='{{ route('place.index')}}'">観光リストへ</button>
+    <button type="button" class="btn btn-success btn-lg" style='margin-bottom: 10px' onclick="location.href='{{ route('index_task_google_calendar')}}'"><img src="../images/icons8-google-calendar-48.png" alt="サンプル画像"width="24" height="24">カレンダーリストへ</button>
 </div>
 <br>
 <h3>私の
@@ -37,10 +38,10 @@
         </label>
     </div>
 </form>
-<button type="button" style='margin-bottom: 10px' onclick="location.href='{{ route('task.create')}}'">新規作成</button>
+<button type="button" class="btn btn-success btn-sm" style='margin-bottom: 10px' onclick="location.href='{{ route('task.create')}}'">新規作成</button>
 <br>
-
-<table cellpadding="10"  width="100%" border="2">
+<div style="background-color : #FFFFFF">
+<table cellpadding="10" class="table table-striped" width="100%" border="2">
     <tr align="center">
         <th nowrap>No.</th>
         <th nowrap>済み</th>
@@ -56,11 +57,13 @@
         <td nowrap style="overflow: hidden; max-width: 0; width: 50%;"><a href="{{ url('/task/show/'.$onetask->id) }}">{{$onetask['task']}}</a></td>
         <td style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden; max-width: 0; width: 20%;">{{$onetask['memo']}}</td>
         <td nowrap>{{$onetask['updated_at']->format('y年m月d日')}}</td>
-        <td nowrap><button type="button" onclick="location.href='{{ route('task.edit', ['id' => $onetask['id']])}}'">編集</button>
-        <button type="button" onclick="location.href='{{ route('task.delete', ['id' => $onetask['id']])}}'">削除</button></td>
+        <td nowrap>
+            <button type="button" class="btn btn-secondary btn-sm" onclick="location.href='{{ route('task.edit', ['id' => $onetask['id']])}}'">編集</button>
+            <button type="button" class="btn btn-danger btn-sm" onclick="location.href='{{ route('task.delete', ['id' => $onetask['id']])}}'">削除</button></td>
     </tr>
     @endforeach
 </table>
+</div>
 <br>
 {{$collection_page->links()}}
 </div>
